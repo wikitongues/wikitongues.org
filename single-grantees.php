424f1c9c95dc32f3d9f1141c06f667a4fda3a26b
Daniel Bogre Udell <daniel@wikitongues.org>
2022-04-10 20:46:08 -0400

    finished grantee single

--- single-grantees.php
+++ single-grantees.php
@@ -25,3 +25,27 @@ include( locate_template('modules/banner.php') );
 		<?php the_field('grantee_description'); ?>
 	</article>
 </main> 
+
+<section class="wt_grantee__navigation">
+	<h1 class="wt_grantee__navigation--title">Other Grantees</h1>
+	<h2 class="wt_grantee__navigation--subheader"> Lorem, ipsum dolor sit amet consectetur, adipisicing elit. Quis eius pariatur, praesentium, dicta veritatis illum.</h2>
+	<?php // testimonials
+		$current_grantee = get_the_ID();
+		$grantees = new WP_Query( 
+			array(
+				'post_type'=>'grantees',
+				'post__not_in' => array( $current_grantee ),
+				'posts_per_page' => 15
+			) 
+		);
+
+		if( $grantees->have_posts() ){		
+			while( $grantees->have_posts() ){
+				$grantees->the_post();
+
+				include( locate_template('modules/grantee-thumbnail.php') );
+			}
+		}
+		wp_reset_postdata();
+	?>
+</section>
