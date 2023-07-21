53f58552968dfdd5cbce8423cce9ae6aee7cc562
Daniel Bogre Udell <daniel@wikitongues.org>
2023-07-21 19:06:39 -0400

    template includes

--- front-page.php
+++ front-page.php
@@ -3,17 +3,23 @@
 // header
 get_header(); // do pseudo code
 
-// banner 
+// banner
+include( get_template('banner.php') );
 
-// thumbnail carousel
+// thumbnail carousel - revitalization projects
+include( get_template('carousel-thumbnail.php') );
 
-// content block - wide
+// language revitalization
+include( get_template('content-block--wide.php') );
 
 // testimonial carousel
+include( get_template('carousel-testimonial.php') );
 
-// content block - wide
+// language archive
+include( get_template('content-block--wide.php') );
 
-// thumbnail carousel
+// thumbnail carousel - archive
+include( get_template('thumbnail-carousel.php') );
 
 // footer
 get_footer();
