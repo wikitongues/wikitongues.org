f7ec084b39de20b9600eae4d237c2107145d5c42
Daniel Bogre Udell <daniel@wikitongues.org>
2023-07-21 19:09:24 -0400

    pseudocode

--- modules/carousel--thumbnail.php
+++ modules/carousel--thumbnail.php
@@ -1,1 +1,6 @@
 <h1>thumbnail carousel module</h1>
+
+<!-- 
+
+	foreach loop, $post type variable set outside template
+		include thumbnail template (all post types will have a 'thumbnail title' field set in the CMS, with a the_title fallback)
