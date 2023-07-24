bd1f8255efec9c56aa734194194199ff4a784508
Daniel Bogre Udell <daniel@wikitongues.org>
2023-07-24 18:02:29 -0400

    shalom olam

---
+++ modules/fellows-single--metadata.php
@@ -0,0 +1,1 @@
+<h1>fellow metadata</h1>
--- single-fellows.php
+++ single-fellows.php
@@ -4,6 +4,7 @@
 get_header();
 
 // banner
+include( get_template('banner--fellows-single.php') );
 
 // content wrap
 	// fellow header and contact information
