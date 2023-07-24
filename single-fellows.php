426033f7de99bf817f63a76dca25f47a8957050a
Daniel Bogre Udell <daniel@wikitongues.org>
2023-07-24 18:05:00 -0400

    typo fix

--- single-fellows.php
+++ single-fellows.php
@@ -8,7 +8,7 @@ include( get_template('modules/banner--fellows-single.php') );
 
 // content
 include( get_template('modules/fellows-single--metadata') );
-include ( get_template('modules/main-content.php' );
+include( get_template('modules/main-content.php' );
 
 // loop through other fellows
 include( get_template('modules/carousal--thumbnail.php') );
