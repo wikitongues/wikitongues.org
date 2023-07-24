b81b6245a4b40ebed25de8d61433ce7074cd00db
Daniel Bogre Udell <daniel@wikitongues.org>
2023-07-24 17:55:39 -0400

    include modules

--- single-languages.php
+++ single-languages.php
@@ -4,19 +4,24 @@
 get_header();
 
 // language single banner
-include( get_template('module/banner--language-single.php') );
+include( get_template('modules/banner--language-single.php') );
 
 // left column language metada
+include( get_template('modules/language-single--metada.php') );
 
 // videos loop (content blocks - thirds)
+include( get_template('modules/content-block--thirds.php') );
 
 // dictionaries (content blocks - thirds)
+include( get_template('modules/content-block--thirds.php') );
 
 // language indexing resources (content blocks - thirds)
+include( get_template('modules/content-block--thirds.php') );
 
-// other posts (revitalization projects, translation/etc, learning options)
+// other posts (revitalization projects, translation/etc, learning options) - add in later version
 
 // other languages (thumbnail carousel)
+include( get_template('modules/carousal--thumbnail.php') );
 
 // footer
 get_footer();
