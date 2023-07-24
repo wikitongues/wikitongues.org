6d5b9a9f3d9af1d86e7c5c9476ffb3f1e965d713
Daniel Bogre Udell <daniel@wikitongues.org>
2023-07-24 18:20:57 -0400

    video single init

---
+++ modules/videos-single--embed.php
@@ -0,0 +1,22 @@
+<h1>videos embed</h1>
+<!-- 
+
+// if video is not private
+
+	// if video file exists
+
+		// video embed
+
+	// if video file does not exist
+
+		// thumbnail embed
+
+	// else
+
+		// output blank element with html note
+
+// else
+
+	statement that video is private. design?
+
+-->
--- single-videos.php
+++ single-videos.php
@@ -4,15 +4,16 @@
 get_header();
 
 // video
-include( get_template('videos-single--embed.php') );
+include( get_template('modules/videos-single--embed.php') );
 
 // left column - video metadata
+include( get_template('modules/meta--videos-single.php') );
 
 // right column - video content
-	// title
-	// description or transcript
+include( get_template('modules/main-content--videos-single.php') );
 
 // related videos (thumbnail carousel)
+include( get_template('modules/carousel--thumbnail.php') );
 
 // footer
 get_footer();
