eb122011986ce6d52ec160694468c94db9c8462c
Daniel Bogre Udell <daniel@wikitongues.org>
2023-07-24 18:07:33 -0400

    renamed modules

--- single-fellows.php
+++ single-fellows.php
@@ -6,8 +6,8 @@ get_header();
 // banner
 include( get_template('modules/banner--fellows-single.php') );
 
-// content
-include( get_template('modules/fellows-single--metadata') );
+// content - consider consolidating?
+include( get_template('modules/meta--fellows-single') );
 include( get_template('modules/main-content.php' );
 
 // loop through other fellows
