3c290585f2b2735cedc2a9225bafba3367c1c24d
Daniel Bogre Udell <daniel@wikitongues.org>
2023-08-06 13:23:44 -0400

    menu/nav markup

--- template-redirect.php
+++ template-redirect.php
@@ -2,4 +2,18 @@
 
 /* Template name: Redirect */
 
-// ACF already exists; build later
+$redirect_to_page = get_field('redirect_to_page');
+$redirect_to_post = get_field('redirect_to_post');
+$custom_redirect = get_field('custom_redirect');
+
+if ( $redirect_to_page ) {
+	header( 'Location: ' . $redirect_to_page );
+}
+
+if ( $redirect_to_post ) {
+	header( 'Location: ' . $redirect_to_post );
+}
+
+if ( $custom_redirect ) {
+	header('Location: ' . $custom_redirect);
+}
