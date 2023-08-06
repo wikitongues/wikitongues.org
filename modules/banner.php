2561ce1b5d137a9e1bd4b07a634375df766daf8b
Daniel Bogre Udell <daniel@wikitongues.org>
2023-08-06 13:43:06 -0400

    markup

--- modules/banner.php
+++ modules/banner.php
@@ -1,1 +1,15 @@
-<h1>banner module</h1>
+<?php
+// vars
+$front_page_banner = get_field('front_page_banner');
+$banner_image = $front_page_banner['banner_image'];
+$banner_header = $front_page_banner['banner_image'];
+$banner_copy = $front_page_banner['banner_copy'];
+$banner_CTA = $front_page_banner['banner_CTA'];
+$banner_CTA_placeholder = $front_page_banner['banner_CTA_placeholder']; 
+?>
+
+<div class="wt_banner" role="img" aria-label="<?php echo $banner_image['alt']; ?>" style="background-image:url(<?php echo $banner_image['url']; ?>);">
+
+
+	
+</div>
