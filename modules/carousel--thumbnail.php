3248876d5a9505cb910b696ff20bb4f6ee8c8154
Daniel Bogre Udell <daniel@wikitongues.org>
2023-08-07 11:48:58 -0400

    markup

--- modules/carousel--thumbnail.php
+++ modules/carousel--thumbnail.php
@@ -2,16 +2,62 @@
 
 <!-- to add later: buttons with carousel js -->
 <?php if ( $post_type ): ?>
-	<ul>
+	<ul class="wt_carousel--thumbnails">
 	<?php foreach ( $post_type as $post ): setup_postdata( $post ); ?>
-		<li>
-			
+	<?php 
+		$thumbnail_title = get_field('thumbnail_title');
+		$thumbnail_cta_text = get_field('thumbnail_cta_text'); 
+		$thumbnail_image = get_field('thumbnail_image');
+		?>
+		<li class="wt_thumbnail">
+		<?php 
+			if ( $thumbnail_image ) {
+
+				echo '<img class="wt_thumbnail__image" src="' . 
+					 $thumbnail_image['url'] .
+					 'alt="' .
+					 $thumbnail_image['alt'] .
+					 '">';
+
+			} else {
+
+				the_post_thumbnail( 'large' );
+
+			} ?>
+
+			<div class="wt_thumbnail__copy">
+			<?php
+
+				if ( $thumbnail_title ) {
+
+					echo '<p>' . $thumbnail_title . '</p>';
+
+				} else {
+
+					echo '<p>' . the_title() . '</p>';
+
+				}
+
+				if ( $thumbnail_cta_text ) {
+
+					echo '<a href="' . the_permalink() . '">' .
+						 $thumbnail_cta_text . 
+						 '</a>'; // add arrow
+
+				} else {
+
+					echo '<a href="' . the_permalink() . '">' .
+						 'Read more</a>'; // add arrow
+
+				}
+
+			?>
+			</div>
 		</li>
 	<?php endforeach; wp_reset_postdata(); ?>
 	</ul>
 <?php endif ?>
 
 <!-- 
-
 	foreach loop, $post type variable set outside template
 		include thumbnail template (all post types will have a 'thumbnail title' field set in the CMS, with a the_title fallback) -->
