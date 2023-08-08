7a0aa2124cdac88968713c62b69c6ff09e2c436b
Daniel Bogre Udell <daniel@wikitongues.org>
2023-08-08 14:31:49 -0400

    defined variables

--- front-page.php
+++ front-page.php
@@ -4,6 +4,12 @@
 get_header(); // do pseudo code
 
 // banner
+$front_page_banner = get_field('front_page_banner');
+$banner_image = $front_page_banner['banner_image'];
+$banner_header = $front_page_banner['banner_header'];
+$banner_copy = $front_page_banner['banner_copy'];
+$banner_CTA;
+
 include( 'modules/banner.php' );
 
 // initiate flexible content loop
