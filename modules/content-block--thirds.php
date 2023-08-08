b727946de2621821edc7d12169233011716805e2
Daniel Bogre Udell <daniel@wikitongues.org>
2023-08-08 16:03:42 -0400

    layout markup

--- modules/content-block--thirds.php
+++ modules/content-block--thirds.php
@@ -1,2 +1,22 @@
-<!-- single element to be looped through -->
-<h1>content block - thirds module</h1>
+<?php
+$content_block_image = get_sub_field('content_block_image');
+$content_block_header = get_sub_field('content_block_header');
+$content_block_copy = get_sub_field('content_block_copy');
+$content_block_cta = get_sub_field('content_block_cta'); 
+?>
+
+<section class="wt_content-block--thirds">
+	<aside class="wt_content-block--thirds__image" role="img" aria-label="<?php echo $content_block_image['alt']; ?>" style="background-image:url(<?php echo $content_block_image['url']; ?>);"></aside>
+	<aside class="wt_content-block--thirds__copy">
+		<h1>
+			<?php echo $content_block_header; ?>
+		</h1>
+		<p>
+			<?php echo $content_block_copy; ?>
+		</p>
+		<!-- consolidate field type -->
+		<a href="<?php echo $content_block_cta['content_block_cta_link']; ?>">
+			<?php echo $content_block_cta['content_block_cta_text']; ?>
+		</a>
+	</aside>
+</section>
