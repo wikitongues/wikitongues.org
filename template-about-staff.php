db67a746985da716fe9f282d16666bda6e9240bc
Daniel Bogre Udell <bogreudell@daniels-imac.mynetworksettings.com>
2023-09-07 15:08:48 -0400

    new field group

--- template-about-staff.php
+++ template-about-staff.php
@@ -3,12 +3,63 @@
 // header
 get_header();
 
-// define staff team member posts to display (acf)
+// team banner
+$team_banner = get_field('team_banner');
 
-// cycle through selected posts
+include( 'modules/banner--team.php' );
+
+// define team member posts to display
+$staff_members = get_field('staff_members');
+
+// cycle through	 selected posts
+foreach( $staff_members as $post ) {
+	// setup post data for each post
+	setup_postdata( $post );
+
+	$profile_picture = get_field('profile_picture');
+	$name = get_the_title();
+	$title = get_field('leadership_title');
+	$bio = get_field('bio');
+	$location = get_field('contributor_location');
+	$linkedin = get_field('linkedin');
+	$website = get_field('website');
+	$twitter = get_field('twitter');
+	$email = get_field('email');
+	$personal_languages = get_field('languages');
+
+	// show board member module
+	include( 'modules/team-member--wide.php' );
+
+} wp_reset_postdata();
+
+get_footer();
+
 
 // define volunteer+intern team member posts to display acf
 
-// cycle through selected posts 
+// cycle through selected posts
+$interns_and_volunteers = get_field('interns_and_volunteers');
+
+// cycle through selected posts
+foreach( $board_members as $post ) {
+	// setup post data for each post
+	setup_postdata( $post );
+
+	// variables - would be cool to add a 
+	$profile_picture = get_field('profile_picture');
+	$name = get_the_title();
+	$title = get_field('leadership_title');
+	$location = get_field('contributor_location');
+	$linkedin = get_field('linkedin');
+	$website = get_field('website');
+	$twitter = get_field('twitter');
+	$email = get_field('email');
+
+	// show board member module
+	include( 'modules/team-member--thirds.php' );
+
+} wp_reset_postdata();
+
+get_footer();
 
 get_footer();
