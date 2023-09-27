34b04de66d1805aa38d737d320da724d54f872c4
bogreudell <daniel@wikitongues.org>
2023-09-26 23:17:23 -0400

    fixed thumbnails

--- modules/content-block--grid.php
+++ modules/content-block--grid.php
@@ -1,6 +1,11 @@
 <section class="wt_content-block--thirds">
 <?php if ( $content_block_image ): ?>
-	<aside class="wt_content-block--thirds__image" role="img" aria-label="<?php echo $content_block_image['alt']; ?>" style="background-image:url(<?php echo $content_block_image['url']; ?>);" data-test="<?php echo $content_block_image; ?>"></aside>
+	<aside
+		class="wt_content-block--thirds__image"
+		role="img"
+		aria-label="<?php echo get_post_meta($content_block_image, '_wp_attachment_image_alt', TRUE); ?>"
+		style="background-image:url(<?php echo wp_get_attachment_url($content_block_image) ?>);">
+	</aside>
 <?php elseif ( !$content_block_image && $post->post_type !== 'lexicons' && $post->post_type !== 'resources' ): ?>
 	<aside class="wt_content-block--thirds__image blank" role="img" aria-label="<?php echo $content_block_image['alt']; ?>" style="background-image:url(<?php echo $content_block_image['url']; ?>);"></aside>
 <?php else: ?>
