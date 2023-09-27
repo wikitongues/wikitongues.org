ad26c64d2acf6ced1e6332e09c3e3b3ccdc2e2f8
bogreudell <daniel@wikitongues.org>
2023-09-26 23:28:40 -0400

    fixed thumbnails

--- modules/search-results__thumbnail.php
+++ modules/search-results__thumbnail.php
@@ -27,6 +27,18 @@ if ( $thumbnail_image ) {
 		 get_post_meta($thumbnail_image, '_wp_attachment_image_alt', TRUE) .
 		 '"></div>';
 
+} elseif ( 'videos' === get_post_type() && !$thumbnail_image ) {
+
+	$video_thumbnail = get_field('video_thumbnail_v2');
+
+	echo '<div role="img" class="wt_search-results__thumbnail--image" style="background-image:url('. 
+		 $video_thumbnail['url'] .
+		 wp_get_attachment_url($video_thumbnail) .
+		 '") aria-label="' .
+		 $video_thumbnail['alt'] .
+		 get_post_meta($video_thumbnail, '_wp_attachment_image_alt', TRUE) .
+		 '"></div>';
+
 } elseif ( has_post_thumbnail() ) { 
 
 	echo '<div role="img" class="wt_search-results__thumbnail--image" style="background-image:url('. 
