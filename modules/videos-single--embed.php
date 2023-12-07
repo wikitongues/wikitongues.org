e9bb88f30f7133a6745c3b30ebd89526ae4ada02
Daniel Bogre Udell <daniel@wikitongues.org>
2023-12-07 18:16:41 -0500

    fixed No ID bug

--- modules/videos-single--embed.php
+++ modules/videos-single--embed.php
@@ -3,13 +3,26 @@
 
 <?php if ( $public_status === 'Public' ): ?>
 
-	<div class="wt_single-videos__video <?php if ( $youtube_id || $youtube_link ): ?>has-iframe<?php endif; ?>">
+	<!-- video wrap, class conditioned on content -->
+	<?php if ( $youtube_id ): ?>
+
+		<div class="wt_single-videos__video has-iframe">
+
+	<?php elseif ( $youtube_link && $youtube_link !== 'No ID' ): ?>
+
+		<div class="wt_single-videos__video has-iframe">
+
+	<?php else: ?>
+
+		<div class="wt_single-videos__video">
+
+	<?php endif; ?>
 
 	<?php if ( $youtube_id ): ?>
 
 		<iframe width="100%" src="https://www.youtube.com/embed/<?php echo $youtube_id; ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
-	<?php elseif ( $youtube_link ): ?>
+	<?php elseif ( $youtube_link && $youtube_link !== 'No ID' ): ?>
 
 		<?php $youtube_id = substr(strrchr($youtube_link, "/"), 1); ?>
 
