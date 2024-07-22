a4ae68186f26f89e1f2d62b80214456054b13549
FredericoAndrade <faandrade@gmail.com>
2024-07-22 11:02:54 +0200

    enable multiple typeaheads

--- modules/banner--searchbar.php
+++ modules/banner--searchbar.php
@@ -1,3 +1,5 @@
 <div class="wt_banner--searchbar">
-	<?php get_search_form(); ?><!-- search form needs to search by fields (i.e., standard name) and only apply to language and videos posts at first. We can then add lexicons, misc., and lastly, language revitalization projects, but that's going to require more work and may be a 2024 reframing		 -->
+	<div id="typeahead-archive" class="react-typeahead-container">
+		[react_typeahead custom_class="body-style"]
+	</div>
 </div>
