b0c6120c9696bc3a8aa0b9f6d5b2828fe45cb121
FredericoAndrade <faandrade@gmail.com>
2024-08-25 18:04:15 +0200

    init 404

---
+++ 404.php
@@ -0,0 +1,6 @@
+<?php get_header(); ?>
+<article id="page-404" class="post not-found">
+  <h1>Not Found</h1>
+  <p>Nothing found for the requested page. <a href="<?php bloginfo('url'); ?>/archive">Try a search instead?</a></p>
+</article>
+<?php get_footer(); ?>
