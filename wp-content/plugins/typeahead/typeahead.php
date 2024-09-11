fa196a90b20966aeed474655587aeb616478571b
FredericoAndrade <faandrade@gmail.com>
2024-09-11 23:03:14 +0200

    wrap full repo

---
+++ wp-content/plugins/typeahead/typeahead.php
@@ -0,0 +1,73 @@
+<?php
+/*
+Plugin Name: Wikitongues React Typeahead
+Description: Integrate Wikitongus React Typeahead with WordPress
+Version: 1.0
+Author: Frederico Andrade
+*/
+
+function react_typeahead_enqueue_scripts() {
+    $plugin_dir = plugin_dir_url(__FILE__);
+
+    // Get the hashed filenames for CSS and JS files
+    $manifest = json_decode(file_get_contents(plugin_dir_path(__FILE__) . 'build/asset-manifest.json'), true);
+    $main_css = $manifest['files']['main.css'];
+    $main_js = $manifest['files']['main.js'];
+
+    // Enqueue the CSS file
+    wp_enqueue_style(
+        'react-typeahead-style',
+        $plugin_dir . 'build/' . $main_css
+    );
+
+    // Enqueue the JavaScript file
+    wp_enqueue_script(
+        'react-typeahead-script',
+        $plugin_dir . 'build/' . $main_js,
+        array(), // Dependencies, if any
+        null, // Version
+        true // Load in footer
+    );
+}
+
+add_action('wp_enqueue_scripts', 'react_typeahead_enqueue_scripts');
+
+function react_typeahead_shortcode($atts) {
+    $atts = shortcode_atts(array(
+        'id' => 'react_typeahead_root_' . uniqid(),
+        'custom_class' => '',
+        'data_source' => 'wordpress'
+    ), $atts, 'react_typeahead');
+
+    $unique_id = esc_attr($atts['id']);
+    $custom_class = esc_attr($atts['custom_class']);
+    $data_source = esc_attr($atts['data_source']);
+
+    $settings = array(
+        'customClass' => $custom_class,
+        'uniqueId' => $unique_id,
+        'dataSource' => $data_source
+    );
+
+    $settings_var_name = 'wpTypeaheadSettings_' . preg_replace('/[^a-zA-Z0-9_]/', '_', $unique_id);
+
+    wp_localize_script('react-typeahead-script', 'wpTypeaheadSettings_' . $unique_id, $settings);
+    return '<div id="' . $unique_id . '" class="react-typeahead-container ' . $custom_class . '"></div>';
+}
+
+add_shortcode('react_typeahead', 'react_typeahead_shortcode');
+
+function hide_shortcode_container() {
+    ?>
+    <script type="text/javascript">
+        document.addEventListener('DOMContentLoaded', function() {
+            let instances = document.querySelectorAll('.react-typeahead-container');
+            instances.forEach(function(instance) {
+                instance.style.display = 'none';
+            });
+        });
+    </script>
+    <?php
+}
+
+add_action('wp_head', 'hide_shortcode_container');
---
+++ wp-content/plugins/wt-gallery/includes/queries.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Retrieves a list of posts for the custom gallery.
+ *
+ * @param array $args Arguments to customize the WP_Query.
+ * @return WP_Query The query result.
+ */
+
+function get_custom_gallery_query($atts = array()) {
+    $defaults = array(
+        'post_type' => 'languages', // videos, languages, fellows
+        'posts_per_page' => 6,
+        'orderby' => 'date',
+        'order' => 'DESC',
+        'meta_key' => '',
+        'meta_value' => '',
+        'paged' => 1,
+    );
+
+    $args = wp_parse_args($atts, $defaults);
+
+    $meta_query = array();
+
+    if (!empty($atts['meta_key']) && !empty($atts['meta_value'])) {
+        $val_array = explode(',',$atts['meta_value']);
+        $val_array = array_map('trim', $val_array);
+
+        if (count($val_array) > 1) {
+            $meta_query = array('relation' => 'OR');
+            foreach ($val_array as $value) {
+                $value = trim($value);
+                if(!empty($value)) {
+                    $meta_query[] = array(
+                        'key' => $atts['meta_key'],
+                        'value' => $value,
+                        'compare' => 'LIKE',
+                    );
+                }
+            }
+        } else {
+            $meta_query[] = array(
+                'key' => $atts['meta_key'],
+                'value' => $atts['meta_value'],
+                'compare' => 'LIKE',
+            );
+        };
+
+        $args['meta_query'] = $meta_query;
+        unset($args['meta_key']);
+        unset($args['meta_value']);
+    }
+
+    // // Exclude current post from the query
+    $current_post_type = get_post_type();
+    if ($current_post_type === $args['post_type']) {
+        $args['post__not_in'] = array(get_the_ID());
+    }
+
+    $query = new WP_Query($args);
+
+    return $query;
+}
+
+/**
+ * Retrieves a random video for a given language ISO code.
+ *
+ * @param string $iso_code The ISO code of the language.
+ * @return WP_Query The query result.
+ */
+function get_videos_by_featured_language($language_title) {
+    // Use WP_Query to find the language post by title
+    $language_query = new WP_Query(array(
+        'post_type' => 'languages', // Adjust this to your custom post type if needed
+        'title' => $language_title,
+        'posts_per_page' => 1, // Only need one post
+        'fields' => 'ids', // Only get the ID to optimize performance
+    ));
+
+    // Check if a language post is found
+    if (!$language_query->have_posts()) {
+        // If no post is found with the given title, return an empty WP_Query
+        return new WP_Query();
+    }
+
+    // Get the language post ID
+    $language_post_id = $language_query->posts[0];
+
+    // Prepare the meta query to match the 'featured_languages' field by the language post ID
+    $args = array(
+        'post_type' => 'videos',
+        'posts_per_page' => 1, // Adjust as needed to control the number of videos returned
+        'orderby' => 'rand', // Random order
+        'meta_query' => array(
+            array(
+                'key' => 'featured_languages', // ACF relationship field
+                'value' => '"' . $language_post_id . '"', // Match against the serialized ID in the array
+                'compare' => 'LIKE', // Partial match to find serialized post IDs
+            ),
+        ),
+    );
+
+    // Execute the query
+    $video_query = new WP_Query($args);
+
+    return $video_query;
+}
