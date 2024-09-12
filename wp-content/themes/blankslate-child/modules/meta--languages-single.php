9ff4c7c4efeca925ecb10aba8f8edc85f9a9daf0
FredericoAndrade <faandrade@gmail.com>
2024-09-12 14:46:43 +0200

    add title to language page meta section official links

--- wp-content/themes/blankslate-child/modules/meta--languages-single.php
+++ wp-content/themes/blankslate-child/modules/meta--languages-single.php
@@ -16,29 +16,32 @@
 	<ul>
 		<li>
 			<a href="#wt_single-languages__videos">
-				<h2>Videos <?php echo ($videos_count > 0 ? '(' . $videos_count . ')' : ''); ?></h2>
+				<h3>Videos <?php echo ($videos_count > 0 ? '(' . $videos_count . ')' : ''); ?></h3>
 			</a>
 			<a href="<?php echo home_url(); ?>/submit-a-video">Submit a video</a>
 		</li>
 		<li>
 			<a href="#wt_single-languages__lexicons">
-				<h2>Dictionaries, phrase books, and lexicons <?php echo ($lexicons_count > 0 ? '(' . $lexicons_count . ')' : ''); ?></h2>
+				<h3>Dictionaries, phrase books, and lexicons <?php echo ($lexicons_count > 0 ? '(' . $lexicons_count . ')' : ''); ?></h3>
 			</a>
 			<a href="<?php echo home_url(); ?>/submit-a-lexicon">Submit a lexicon</a>
 		</li>
 		<li>
 			<a href="#wt_single-languages__resources">
-				<h2>External Resources <?php echo ($external_resources_count > 0 ? '(' . $external_resources_count . ')' : ''); ?></h2>
+				<h3>External Resources <?php echo ($external_resources_count > 0 ? '(' . $external_resources_count . ')' : ''); ?></h3>
 			</a>
 			<a href="<?php echo home_url(); ?>/submit-a-resource">Recommend a resource</a>
 		</li>
 	</ul>
+	<h2>Learn more about <?php  echo $standard_name; ?></h2>
+	<ul>
 	<?php
 		foreach ($links as $key => $value) {
 			if (!empty($value)) {
-				echo '<a class="official-link" href="' . esc_url($value) . '" target="_blank">' . ucfirst($key) . '</a>';
+				echo '<li><a class="official-link" href="' . esc_url($value) . '" target="_blank">' . ucfirst($key) . '</a></li>';
 			}
 		}
 	?>
+	</ul>
 	<a href="https://abdbdjge.donorsupport.co/-/XTRAFEBU" class="donate-cta">Support Language Revitalization</a>
 </div>
