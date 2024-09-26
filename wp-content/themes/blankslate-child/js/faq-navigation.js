916f828f80637f9cd57a0a5a968763ea2ce34339
FredericoAndrade <faandrade@gmail.com>
2024-09-26 15:38:13 +0200

    setup faq page

---
+++ wp-content/themes/blankslate-child/js/faq-navigation.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function($) {
+	let sections = $('h2[id]');
+	let navigationLinks = $('.faq-navigation a');
+
+	$(window).on('scroll', function() {
+			let currentScroll = $(this).scrollTop();
+			let currentSection;
+
+			sections.each(function() {
+					let sectionTop = $(this).offset().top - 120; // Adjust offset as needed
+					if (currentScroll >= sectionTop) {
+							currentSection = $(this);
+					}
+			});
+
+			if (currentSection) {
+					let id = currentSection.attr('id');
+					navigationLinks.removeClass('active');
+					$('.faq-navigation a[href="#' + id + '"]').addClass('active');
+			}
+	});
+});
