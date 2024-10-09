7573e45a4ae47789f78a489081609eeabd24d797
FredericoAndrade <faandrade@gmail.com>
2024-10-10 00:17:51 +0200

    sets up resources gallery

--- wp-content/themes/blankslate-child/modules/single-languages__resources.php
+++ wp-content/themes/blankslate-child/modules/single-languages__resources.php
@@ -1,35 +1,32 @@
 <div id="wt_single-languages__resources" class="wt_single-languages__contents">
-	<h2 class="wt_sectionHeader">External resources</h2>
-	<?php if ( $external_resources ): ?>
-		<ul>
+	<?php
+	if (!function_exists('getDomainFromUrl')) {
+    function getDomainFromUrl($url) {
+        $host = parse_url($url, PHP_URL_HOST);
+        if (substr($host, 0, 4) === 'www.') {
+            $host = substr($host, 4);
+        }
+        return $host;
+    }
+}
+	?>
 		<?php
-		// loop through available lexicons
-		foreach( $external_resources as $post ) {
-			// foreach video, setup posts data
-			setup_postdata( $post );
-
-			// define hard variables
-			$resource_post_title = get_the_title();
-			$resource_custom_title = get_field('resource_title');
-			$content_block_image = null;
-			$content_block_copy = null;
-			$content_block_cta_link = get_field('resource_url');
-			$content_block_cta_text = 'View Resource';
-
-			// define conditional variables
-			if ( $resource_custom_title ) {
-				$content_block_header = $resource_custom_title;
-			} else {
-				$content_block_header = get_the_title();
-			}
-
-			// include content block template
-			include( 'content-block--grid.php' );
-
-		} wp_reset_postdata();
+			$custom_title = 'External Resources';
+			$custom_post_type = 'resources';
+			$custom_class = '';
+			$custom_columns = 3;
+			$custom_posts_per_page = 6;
+			$custom_orderby = 'date';
+			$custom_order = 'asc';
+			$custom_pagination = 'true';
+			$custom_meta_key = 'resource_language';
+			$custom_meta_value = $language;
+			$custom_selected_posts = '';
+			echo do_shortcode('[custom_gallery title="'.$custom_title.'" custom_class="'.$custom_class.'" post_type="'.$custom_post_type.'" columns="'.$custom_columns.'" posts_per_page="'.$custom_posts_per_page.'" orderby="'.$custom_orderby.'" order="'.$custom_order.'" pagination="'.$custom_pagination.'" meta_key="'.$custom_meta_key.'" meta_value="'.$custom_meta_value.'" selected_posts="'.$custom_selected_posts.'"]');
 		?>
-		</ul>
-	<?php else: ?>
-		<p>There are no resources to display—yet. <a href="<?php echo home_url() ;?>/submit-a-resource">Recommend a resource</a>.</p>
-	<?php endif; ?>
+		<div class="custom-cta-container">
+			<section class="custom-gallery-video-cta">
+				<a href="<?php echo home_url(); ?>/submit-a-video">Contribute a resource</a>
+			</section>
+		</div>
 </div>
