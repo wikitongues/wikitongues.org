442295ec6cd1a17dd078a0a20a96d3f0eb8840c8
FredericoAndrade <faandrade@gmail.com>
2024-10-31 12:29:56 +0100

    sets up events feature

--- wp-content/themes/blankslate-child/functions.php
+++ wp-content/themes/blankslate-child/functions.php
@@ -8,3 +8,4 @@ require_once('includes/template-helpers.php');
 require_once('includes/rest-endpoints.php');
 require_once('includes/batch-operations.php');
 require_once('includes/admin-helpers.php');
+require_once('includes/events-filter.php');
