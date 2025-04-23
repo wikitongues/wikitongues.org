63d6ebd34181b52c0d595d3fb6fd4902441ec800
FredericoAndrade <faandrade@gmail.com>
2025-04-23 23:10:30 +0200

    make language wikipedia editions a repeater, update video admin, add video metadata with width and height.

--- wp-content/plugins/wt-gallery/includes/templates/gallery-videos.php
+++ wp-content/plugins/wt-gallery/includes/templates/gallery-videos.php
@@ -1,7 +1,13 @@
 <li class="gallery-item">
   <?php
     $url = get_permalink();
+    $public_status = get_field('public_status');
     $thumbnail_url = get_custom_image('videos');
+    if ($public_status === 'Public') {
+      $thumbnail_url = $thumbnail_url;
+    } else {
+      $thumbnail_url = home_url('/wp-content/uploads/2025/04/wikitongues_processing_thumbnail.png');
+    }
     $thumbnail_object = '';
 
     if ($thumbnail_url) {
