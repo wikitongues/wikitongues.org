de32a1f2513b6215abb879f692b622a8415e62bb
FredericoAndrade <faandrade@gmail.com>
2026-04-13 14:03:28 +0200

    restyle notifications section

--- wp-content/themes/blankslate-child/modules/newsletter.php
+++ wp-content/themes/blankslate-child/modules/newsletter.php
@@ -3,30 +3,45 @@
 	$id               = get_field( 'mailchimp_newsletter_id', 'options' );
 	$newsletter_title = get_field( 'block_title', 'options' );
 	$newsletter_copy  = get_field( 'block_copy', 'options' );
+	$background_image = get_field( 'background_image', 'options' );
 ?>
-<section class="wt_newsletter">
-	<h2><?php echo $newsletter_title; ?></h2>
-	<p><?php echo $newsletter_copy; ?></p>
-	<div id="mc_embed_signup">
-		<label for="mce-EMAIL">Your email</label>
-		<?php
-			echo '<form action="https://wikitongues.us9.list-manage.com/subscribe/post?u=' . $user . '&amp;id=' . $id . '" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate="novalidate">'
-		?>
-			<div id="mc_embed_signup_scroll">
-				<div class="mc-field-group">
-					<input type="email" value="" name="EMAIL" placeholder="hello@wikitongues.org" class="required email" id="mce-EMAIL" aria-required="true">
+<section class="wt_newsletter" style="background-image: url(<?php echo $background_image; ?>);">
+	<div id="content">
+		<h2><?php echo $newsletter_title; ?></h2>
+		<p><?php echo $newsletter_copy; ?></p>
+		<div id="mc_embed_signup">
+			<label for="mce-EMAIL">Your email</label>
+			<?php
+				echo '<form action="https://wikitongues.us9.list-manage.com/subscribe/post?u=' . $user . '&amp;id=' . $id . '" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate="novalidate">'
+			?>
+				<div id="mc_embed_signup_scroll">
+					<div class="mc-field-group">
+						<input type="email" value="" name="EMAIL" placeholder="hello@wikitongues.org" class="required email" id="mce-EMAIL" aria-required="true">
+					</div>
+					<div id="mce-responses" class="clear">
+						<div class="response" id="mce-error-response" style="display:none"></div>
+						<div class="response" id="mce-success-response" style="display:none"></div>
+					</div>
+					<div style="position: absolute; left: -5000px;" aria-hidden="true">
+						<?php echo '<input type="text" name="b_' . $user . '_' . $id . '" tabindex="-1" value="">'; ?>
+					</div>
+					<div class="clear">
+						<button type="submit" name="subscribe" id="mc-embedded-subscribe" class="button wt_cta">Stay informed</button>
+					</div>
+					<script>
+						const emailInput = document.getElementById('mce-EMAIL');
+						const submitButton = document.getElementById('mc-embedded-subscribe');
+
+						function toggleButtonDisabled() {
+							submitButton.disabled = emailInput.value.trim() === '';
+						}
+
+						emailInput.addEventListener('input', toggleButtonDisabled);
+						toggleButtonDisabled();
+					</script>
+					</div>
 				</div>
-				<div id="mce-responses" class="clear">
-					<div class="response" id="mce-error-response" style="display:none"></div>
-					<div class="response" id="mce-success-response" style="display:none"></div>
-				</div>
-				<div style="position: absolute; left: -5000px;" aria-hidden="true">
-					<?php echo '<input type="text" name="b_' . $user . '_' . $id . '" tabindex="-1" value="">'; ?>
-				</div>
-				<div class="clear">
-					<button type="submit" name="subscribe" id="mc-embedded-subscribe" class="button wt_cta">Stay informed</button>
-				</div>
-			</div>
-		</form>
+			</form>
+		</div>
 	</div>
 </section>
